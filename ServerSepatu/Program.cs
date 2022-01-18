76c77b74d9340a578e25f90b8a1a6d1957d509cc
Yoga Paripurna <63484583+YogaParipurna@users.noreply.github.com>
2022-01-18 10:37:36 +0700

    Add project files.

---
+++ ServerSepatu/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceSepatu;
+
+namespace ServerSepatu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Sepatu));
+                hostObj.Open();
+                Console.WriteLine("Server is Readyyyyy!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
